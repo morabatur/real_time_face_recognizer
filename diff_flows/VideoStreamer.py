86383a932e68299f6bec56e5d7c43ad50d973809
Roman Chernysh <morabatur@gmail.com>
2020-10-17 02:34:00 +0300

    add text

--- diff_flows/VideoStreamer.py
+++ diff_flows/VideoStreamer.py
@@ -1,7 +1,9 @@
 import cv2
 import zmq
 import dlib
+from datetime import datetime
 from time import time
+
 
 
 cap = cv2.VideoCapture(0)
@@ -25,6 +27,10 @@ while True:
         x2 = face.right()
         y2 = face.bottom()
         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        cv2.putText(frame, "FACE", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    cv2.putText(frame, "{} face found".format(len(faces)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    ts = datetime.fromtimestamp(time())
+    cv2.putText(frame, str(ts), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
     faceRecognitionSocket.send_pyobj(dict(grayFrame=grayFrame, faces=faces))
     videoStreamSocket.send_pyobj(frame)
 
